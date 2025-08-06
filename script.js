e4d4d2c194ee32dd4c7d8f7dd6ef78eb9dfa5ba7
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-08-06 04:29:41 +0200

    implement init function with loading and rendering

--- script.js
+++ script.js
@@ -2,3 +2,9 @@ let currentPokemon = 1;
 let allPokemon = [];
 let filteredPokemon = [];
 let isLoading = false;
+async function init() {
+  toggleLoading(true);
+  await loadPokemon(20);
+  renderPokemon();
+  toggleLoading(false);
+}
