cc89c35a26a5d9b31ed85e9e8f824032d9723996
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-08-06 03:57:35 +0200

    add type badge rendering for each Pokémon

--- scripts/template.js
+++ scripts/template.js
@@ -1,24 +1,3 @@
-function getOverlayPokemonTemplate(pokemon, typesHTML, bgColor) {
-    return `
-        <div class="pokemon-overlay-content" style="--bg-color: ${bgColor}">
-            ...
-            <div class="image">
-                <img src="${pokemon.image}" alt="${pokemon.name}" />
-            </div>
-            ...
-        </div>
-    `;
-}
-function getOverlayAboutTemplate(pokemon, abilities) {
-    return `
-        <table id="overlay-bottom-content-table">
-            <tr><td>Height:</td><td>${pokemon.height} m</td></tr>
-            <tr><td>Weight:</td><td>${pokemon.weight} kg</td></tr>
-            <tr><td>Abilities:</td><td>${abilities}</td></tr>
-        </table>
-    `;
-}
-function getOverlayStatsTemplate(stats) {
-    const statBars = stats.map(...).join("");
-    return `<table id="overlay-bottom-content-table">${statBars}</table>`;
+function getPokemonTypesTemplate(type) {
+    return `<div class="pokemon-type">${capitalizeFirstLetter(type.type.name)}</div>`;
 }
