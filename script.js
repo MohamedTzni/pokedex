0375a7a5922cf693391d63e9fdbc1ee535fa4a88
Mohamed Touzani <mohamed.touzani@gmx.de>
2025-08-06 04:30:13 +0200

    add 'loadMore' function to fetch and render additional Pokémon

--- script.js
+++ script.js
@@ -2,7 +2,15 @@ let currentPokemon = 1;
 let allPokemon = [];
 let filteredPokemon = [];
 let isLoading = false;
+
 async function init() {
+  toggleLoading(true);
+  await loadPokemon(20);
+  renderPokemon();
+  toggleLoading(false);
+}
+async function loadMore() {
+  if (isLoading) return;
   toggleLoading(true);
   await loadPokemon(20);
   renderPokemon();
